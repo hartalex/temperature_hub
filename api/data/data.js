2f3b9ed8fb6fe124693dbf9cbe5c655cf7da5fe9
Alex Hart <alex@hartcode.com>
2017-02-21 14:24:21 -0600

    adjust tabs

--- api/data/data.js
+++ api/data/data.js
@@ -1,54 +1,55 @@
 const db = require('../db/mongodb')
 const dbUrl = require('../db/url')
 
-module.exports = { dataAdd: function (input, output) {
-  output.result = 'fail'
+module.exports = {
+  dataAdd: function (input, output) {
+    output.result = 'fail'
   // Use connect method to connect to the Server
-  db.connect(dbUrl, function (err, dbobj) {
-    const time = new Date()
-    if (err == null) {
-      console.log(input)
-      if (typeof input === 'undefined') {
-        console.log('Error request body is undefined')
-        output.reason = 'Request Body is Undefined'
-      } else {
+    db.connect(dbUrl, function (err, dbobj) {
+      const time = new Date()
+      if (err == null) {
+        console.log(input)
+        if (typeof input === 'undefined') {
+          console.log('Error request body is undefined')
+          output.reason = 'Request Body is Undefined'
+        } else {
       // temperatures
-        if ('id' in input) {
-          if (typeof input.id === 'string') {
-            if (input.id.length > 0) {
-              if ('t' in input) {
-                if (typeof input.t === 'number') {
-                  var temperature = {}
-                  temperature.sensorId = input.id
-                  temperature.tempInFarenheit = input.t
-                  if (!('utc_timestamp' in input)) {
-                    temperature.utc_timestamp = time.toISOString()
-                  } else {
-                    temperature.utc_timestamp = input.utc_timestamp
-                  }
-                  db.insertData(dbobj, 'temperatures', temperature, function (result) {
-                    dbobj.close()
-                    if (result != null && result.result.n > 0) {
-                      output.result = 'ok'
+          if ('id' in input) {
+            if (typeof input.id === 'string') {
+              if (input.id.length > 0) {
+                if ('t' in input) {
+                  if (typeof input.t === 'number') {
+                    var temperature = {}
+                    temperature.sensorId = input.id
+                    temperature.tempInFarenheit = input.t
+                    if (!('utc_timestamp' in input)) {
+                      temperature.utc_timestamp = time.toISOString()
+                    } else {
+                      temperature.utc_timestamp = input.utc_timestamp
                     }
-                  })
+                    db.insertData(dbobj, 'temperatures', temperature, function (result) {
+                      dbobj.close()
+                      if (result != null && result.result.n > 0) {
+                        output.result = 'ok'
+                      }
+                    })
+                  } else {
+                    console.log('Error t property is not a number')
+                    output.reason = 'Property t is not a number'
+                  }
                 } else {
-                  console.log('Error t property is not a number')
-                  output.reason = 'Property t is not a number'
+                  console.log('Error json object is missing the t property')
+                  output.reason = 'Missing t property'
                 }
               } else {
-                console.log('Error json object is missing the t property')
-                output.reason = 'Missing t property'
+                console.log('Error id property cannot be empty')
+                output.reason = 'Property id is an empty string'
               }
             } else {
-              console.log('Error id property cannot be empty')
-              output.reason = 'Property id is an empty string'
+              console.log('Error id property is not a string')
+              output.reason = 'Property id is not a string'
             }
-          } else {
-            console.log('Error id property is not a string')
-            output.reason = 'Property id is not a string'
-          }
-        } else
+          } else
       // door data
       if ('sensorId' in input) {
         if (typeof input.sensorId === 'string') {
@@ -97,11 +98,11 @@ module.exports = { dataAdd: function (input, output) {
         console.log('Error json object is missing the sensorId property')
         output.reason = 'Property id is missing'
       }
+        }
+      } else {
+        console.log('Error connecting to mongo db')
+        console.log(err)
       }
-    } else {
-      console.log('Error connecting to mongo db')
-      console.log(err)
-    }
-  })
-}
+    })
+  }
 }
