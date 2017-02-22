ed5bdca1c1740904aa63bacc00a3535eedfa61e3
Alex Hart <alex@hartcode.com>
2017-02-22 15:19:57 -0600

    correct db conns for sensor add and service add

--- api/routes/sensor_add.js
+++ api/routes/sensor_add.js
@@ -3,12 +3,14 @@ const dbUrl = require('../db/url')
 
 module.exports = function (req, res) {
   // Use connect method to connect to the Server
-  db.connect(dbUrl, function (err, dbobj) {
-    if (err == null) {
+  var connectPromise = db.connect(dbUrl)
+  connectPromise.then(function (dbobj) {
+    return new Promise(function (resolve, reject) {
       var svc = req.body
       if (typeof svc === 'undefined') {
         console.log('Error request body is undefined')
-        res.json({
+        dbobj.close()
+        reject({
           result: 'fail',
           reason: 'Request Body is Undefined'
         })
@@ -28,24 +30,20 @@ module.exports = function (req, res) {
                           name: svc.name
                         }, 'sensors', function (result) {
                           if (result == null) {
-                            db.insert(dbobj, 'sensors', svc, function (result) {
-                              console.log(result)
-                              if (result != null && result.result.n > 0) {
-                                res.json({
-                                  result: 'ok'
-                                })
-                              } else {
-                                res.status(500)
-                                res.json({
-                                  result: 'fail'
-                                })
-                              }
+                            var insertPromise = db.insertData(dbobj, 'sensors', svc)
+                            insertPromise.then(function (result) {
                               dbobj.close()
+                              console.log('object inserted')
+                              resolve(result)
+                            }).catch(function (err) {
+                              console.log('object not inserted')
+                              dbobj.close()
+                              reject(err)
                             })
                           } else {
                             console.log('Error name already exists')
-                            res.status(500)
-                            res.json({
+                            dbobj.close()
+                            reject({
                               result: 'fail',
                               reason: 'Sensor already exists'
                             })
@@ -53,8 +51,8 @@ module.exports = function (req, res) {
                         })
                       } else {
                         console.log('Error sensorID already exists')
-                        res.status(500)
-                        res.json({
+                        dbobj.close()
+                        reject({
                           result: 'fail',
                           reason: 'Sensor already exists'
                         })
@@ -62,60 +60,66 @@ module.exports = function (req, res) {
                     })
                   } else {
                     console.log('Error name property cannot be empty')
-                    res.status(500)
-                    res.json({
+                    dbobj.close()
+                    reject({
                       result: 'fail',
                       reason: 'Property name is an empty string'
                     })
                   }
                 } else {
                   console.log('Error name property is not a string')
-                  res.status(500)
-                  res.json({
+                  dbobj.close()
+                  reject({
                     result: 'fail',
                     reason: 'Property name is not a string'
                   })
                 }
               } else {
                 console.log('Error json object is missing the name property')
-                res.status(500)
-                res.json({
+                dbobj.close()
+                reject({
                   result: 'fail',
                   reason: 'Missing name property'
                 })
               }
             } else {
               console.log('Error sensorId property cannot be empty')
-              res.status(500)
-              res.json({
+              dbobj.close()
+              reject({
                 result: 'fail',
                 reason: 'Property sensorId is an empty string'
               })
             }
           } else {
             console.log('Error sensorId property is not a string')
-            res.status(500)
-            res.json({
+            dbobj.close()
+            reject({
               result: 'fail',
               reason: 'Property sensorId is not a string'
             })
           }
         } else {
           console.log('Error json object is missing the sensorId property')
-          res.status(500)
-          res.json({
+          dbobj.close()
+          reject({
             result: 'fail',
             reason: 'Property sensorId is missing'
           })
         }
       }
-    } else {
-      console.log('Error connecting to mongo db')
-      console.log(err)
-      res.status(500)
-      res.json({
-        result: 'fail'
+    }).then(function (result) {
+      return new Promise(function (resolve, reject) {
+        console.log('handle results')
+        console.log(result)
+        if (result != null && result.result.n > 0) {
+          res.json({result: 'ok'})
+        } else {
+          reject('result was not inserted to database')
+        }
       })
-    }
+    }).catch(function (err) {
+      res.status(500)
+      res.json(err)
+    })
   })
 }
