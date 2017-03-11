67edaad082f148417dd06e80875ff39c8b243f86
alex <alex@hartcode.com>
2017-03-11 08:01:47 -0600

    Start of bringing in react stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,10 @@ module.exports = {
   "extends": "standard",
   "plugins": [
     "standard",
-    "promise"
+    "promise",
+    "react",
+    "jsx-a11y",
+    "import"
   ],
   "globals": { "fetch": false, "describe": false, "it": false }
 };
