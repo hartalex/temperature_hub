4131120dc88023ba8e3f8fa9c4095deedc775a8b
Alex Hart <alex@hartcode.com>
2017-03-16 15:26:50 -0500

    add cors for zscaler

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 var express = require('express')
 var apiRoutes = require('../api/routes/routes')
+var cors = require('cors')
 import webpackDevMiddleware from 'webpack-dev-middleware'
 import webpackHotMiddleware from 'webpack-hot-middleware'
 import webpack from 'webpack'
 import config from '../webpack.config'
 const port = process.env.PORT || 80
 const app = express()
+
+var whitelist = ['http://hub.hartcode.com', 'https://gateway.zscaler.net']
+var corsOptions = {
+  origin: function (origin, callback) {
+    var originIsWhitelisted = whitelist.indexOf(origin) !== -1
+    callback(originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted)
+  }
+}
+
+app.use(cors(corsOptions))
 
 if (process.env.NODE_ENV !== 'production') {
   const compiler = webpack(config)
