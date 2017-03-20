213301971546df326ef93fd16c2ee9102ecb4c04
Alex Hart <alex@hartcode.com>
2017-03-20 16:19:00 -0500

    bring in layers that don't change ever

--- web/components/weatherMapComponent.js
+++ web/components/weatherMapComponent.js
@@ -36,19 +36,17 @@ class WeatherMapComponent extends React.Component {
     if (this.state.data !== null) {
       retval = (
         <div style={this.state.style}>
-          <div id='ctl00_ctl00_master_body_map_basemap' style={{ width: this.state.imageWidth, height: this.state.imageHeight, backgroundSize: this.state.imageWidth + ' ' + this.state.imageHeight, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundImage: 'url(http://images.intellicast.com/images/basemaps/1km/msn.jpg)' }}>
-            <div id='ctl00_ctl00_master_body_map_weather' style={{ width: this.state.imageWidth, height: this.state.imageHeight, backgroundSize: this.state.imageWidth + ' ' + this.state.imageHeight, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundImage: 'url(http://images.intellicast.com/WxImages/1kmStormWatchLoop/msn_None_anim.gif)' }}>
-              <div id='ctl00_ctl00_master_body_map_counties' style={{ width: this.state.imageWidth, height: this.state.imageHeight, backgroundSize: this.state.imageWidth + ' ' + this.state.imageHeight, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundImage: 'url(http://images.intellicast.com/images/Overlays/1km/counties/msn.gif)' }}>
-                <div id='ctl00_ctl00_master_body_map_highways' style={{ width: this.state.imageWidth, height: this.state.imageHeight, backgroundSize: this.state.imageWidth + ' ' + this.state.imageHeight, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundImage: 'url(http://images.intellicast.com/images/Overlays/1km/highways/msn.gif)' }}>
+          <div id='ctl00_ctl00_master_body_map_basemap' style={{ width: this.state.imageWidth, height: this.state.imageHeight, backgroundSize: this.state.imageWidth + ' ' + this.state.imageHeight, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundImage: 'url(../weatherMaps/baseMap.jpg)' }}>
+              <div id='ctl00_ctl00_master_body_map_counties' style={{ width: this.state.imageWidth, height: this.state.imageHeight, backgroundSize: this.state.imageWidth + ' ' + this.state.imageHeight, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundImage: 'url(../weatherMaps/counties.gif)' }}>
+                <div id='ctl00_ctl00_master_body_map_highways' style={{ width: this.state.imageWidth, height: this.state.imageHeight, backgroundSize: this.state.imageWidth + ' ' + this.state.imageHeight, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundImage: 'url(../weatherMaps/highways.gif)' }}>
                   <div id='ctl00_ctl00_master_body_map_watchboxes' style={{ width: this.state.imageWidth, height: this.state.imageHeight, backgroundSize: this.state.imageWidth + ' ' + this.state.imageHeight, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundImage: 'url(http://images.intellicast.com/WxImages/1kmWatchBoxes/msn.gif)' }}>
-                    <div id='ctl00_ctl00_master_body_map_cities' style={{ width: this.state.imageWidth, height: this.state.imageHeight, backgroundSize: this.state.imageWidth + ' ' + this.state.imageHeight, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundImage: 'url(http://images.intellicast.com/images/Overlays/1km/cities/msn.gif)' }}>
+                    <div id='ctl00_ctl00_master_body_map_cities' style={{ width: this.state.imageWidth, height: this.state.imageHeight, backgroundSize: this.state.imageWidth + ' ' + this.state.imageHeight, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundImage: 'url(../weatherMaps/cities.gif)' }}>
                       <img id='map' src='http://images.intellicast.com/WxImages/1kmStormWatchLoop/msn_None_anim.gif' alt='' width={this.state.imageWidth} height={this.state.imageHeight} style={{width: this.state.imageWidth, height: this.state.imageHeight}} useMap='#onekmMap' />
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
         </div>)
     } else {
       retval = (<div style={this.state.style}>Fetching Data</div>)
