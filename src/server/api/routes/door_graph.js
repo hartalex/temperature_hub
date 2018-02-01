266a806b66ff7d87f513d5c4f7f392d7ee222918
Alex Hart <alexhart@occinc.com>
2018-02-01 16:03:43 -0600

    Refactor door_graph queries

--- src/server/api/routes/door_graph.js
+++ src/server/api/routes/door_graph.js
@@ -33,52 +33,22 @@ function getAggregateQuery (lastOldestTime, timeStampCompareLength) {
 }
 
 var finddoorsLastXMonths = function (dbobj, x, callback) {
-  const currentTime = new Date()
-  const lastOldestTime = new Date(currentTime - (3600 * 24 * 30 * x * 1000)).toISOString()
-  const timeStampCompareLength = 16
-  db.queryAggregateData(dbobj, getAggregateQuery(lastOldestTime, timeStampCompareLength), 'doors',
-  function (objs) {
-    var firstObjects = []
-    for (var i = 0; i < objs.length; i++) {
-      if (i === 0) {
-        var obj = JSON.parse(JSON.stringify(objs[i]))
-        obj._id.minute = lastOldestTime
-        for (var x = 0; x < obj.results.length; x++) {
-          obj.results[x].isOpen = !(obj.results[x]).isOpen
-        }
-        firstObjects.push(obj)
-      }
-    }
-    callback(firstObjects.concat(objs))
-  })
+  const lastOldestTime = new Date(new Date() - (3600 * 24 * 30 * x * 1000)).toISOString()
+  doAggregateQuery(dbobj, x, callback, lastOldestTime)
 }
 
 var finddoorsLastXDays = function (dbobj, x, callback) {
-  const currentTime = new Date()
-  const lastOldestTime = new Date(currentTime - (3600 * 24 * x * 1000)).toISOString()
-  const timeStampCompareLength = 16
-  db.queryAggregateData(dbobj, getAggregateQuery(lastOldestTime, timeStampCompareLength), 'doors',
-  function (objs) {
-    var firstObjects = []
-    for (var i = 0; i < objs.length; i++) {
-      if (i === 0) {
-        var obj = JSON.parse(JSON.stringify(objs[i]))
-        obj._id.minute = lastOldestTime
-        for (var x = 0; x < obj.results.length; x++) {
-          obj.results[x].isOpen = !(obj.results[x]).isOpen
-        }
-        firstObjects.push(obj)
-      }
-    }
-    callback(firstObjects.concat(objs))
-  })
+  const lastOldestTime = new Date(new Date() - (3600 * 24 * x * 1000)).toISOString()
+  doAggregateQuery(dbobj, x, callback, lastOldestTime)
 }
 
 var finddoorsLastXHours = function (dbobj, x, callback) {
-  const currentTime = new Date()
-  const lastOldestTime = new Date(currentTime - (3600 * x * 1000)).toISOString()
-  const timeStampCompareLength = 16
-  db.queryAggregateData(dbobj, getAggregateQuery(lastOldestTime, timeStampCompareLength), 'doors',
+  const lastOldestTime = new Date(new Date() - (3600 * x * 1000)).toISOString()
+  doAggregateQuery(dbobj, x, callback, lastOldestTime)
+}
+
+var doAggregateQuery = function (dbobj,x, callback, lastOldestTime) {
+  db.queryAggregateData(dbobj, getAggregateQuery(lastOldestTime, 16), 'doors',
   function (objs) {
     var firstObjects = []
     for (var i = 0; i < objs.length; i++) {
