4b29694a7635528f973b3b2f180be91a594167eb
Alex Hart <alex@hartcode.com>
2018-02-04 21:28:04 -0600

    fix red color

--- src/client/alertCheck.js
+++ src/client/alertCheck.js
@@ -1,6 +1,7 @@
 import Colors from './colors'
 
 module.exports = function(that, alertCheckInterval) {
+  return () => {
   var color
   if (new Date() - new Date(that.state.data.lastUpdate) > alertCheckInterval) {
     color = Colors.Red
@@ -11,4 +12,5 @@ module.exports = function(that, alertCheckInterval) {
   styleClone.backgroundColor = color
   that.state.style = styleClone
   that.setState(that.state)
+}
 }
