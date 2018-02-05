712a6701ff2b356728715e26b53734571ddf4585
Alex Hart <alexhart@occinc.com>
2018-02-05 14:29:58 -0600

    setup redux for tempComponents

--- src/client/components/tempComponent.js
+++ src/client/components/tempComponent.js
@@ -1,27 +1,12 @@
+import { connect } from 'react-redux'
 import React from 'react'
 import Colors from '../colors'
 import Util from '../util'
 import temperatureColor from '../temperatureColor'
 import PropTypes from 'prop-types'
-import ClientConfig from '../config.js'
-import AlertCheck from '../alertCheck'
 
-class TemperatureComponent extends React.Component {
-  constructor (props, graphId, getData) {
-    super(props)
-    var updateInterval = props.updateIntervalInMinutes * 60000
-    if (updateInterval === 0) {
-      updateInterval = 60000
-    }
-    var alertCheckInterval = updateInterval * 1.5
-    var renderInterval = 60000
-    this.state = {
-      data: {
-        name: props.sensorName,
-        temperature: 0,
-        lastUpdate: '2017-01-01T00:00:00.000Z',
-        outdated: true
-      },
+const TemperatureComponent = ({sensorName, myTempData}) => {
+    var state = {
       style: {
         width: '140px',
         height: '200px',
@@ -36,69 +21,56 @@ class TemperatureComponent extends React.Component {
         padding: '50px 0'
       }
     }
-    setInterval(AlertCheck(this, alertCheckInterval), renderInterval)
-    this.getData(props.sensorName, this)
-    setInterval(() => { this.getData(props.sensorName, this) }, updateInterval)
-  }
-  getData (sensorName, obj) {
-    fetch(ClientConfig.hub_api_url + '/temp/current').then(function (response) {
-      if (response.status >= 400) {
-        throw new Error('Bad response from server, ' + response.status)
-      }
-      return response.json()
-    }).then(function (currentjson) {
-      for (var i = 0; i < currentjson.length; i++) {
-        var sensor = currentjson[i]
-        if (sensor.sensorName === sensorName) {
-          var newState = obj.state
-          newState.data.outdated = sensor.outdated
-          if (!sensor.outdated) {
-            newState.data.temperature = sensor.tempInFarenheit
-          } else {
-            newState.data.temperature = 0
-          }
-          newState.data.lastUpdate = new Date().toISOString()
-
-          var styleClone = JSON.parse(JSON.stringify(newState.style))
-          styleClone.color = temperatureColor(newState.data.temperature)
-          newState.style = styleClone
-          obj.setState(newState)
-        }
-      }
-    })
-  }
-  render () {
     var retval
-    if (this.state.data !== null) {
-      const updateTimeInMinutes = Util.timeAgo(this.state.data.lastUpdate)
-      var temp = Math.trunc(this.state.data.temperature)
-      var tempDecimal = Math.abs(Math.trunc((this.state.data.temperature - temp) * 100))
-      if (this.state.data.temperature === 0) {
+      const updateTimeInMinutes = Util.timeAgo(myTempData.lastUpdate)
+      var temp = Math.trunc(myTempData.tempInFarenheit)
+      var tempDecimal = Math.abs(Math.trunc((myTempData.tempInFarenheit - temp) * 100))
+      state.style.color = temperatureColor(tempDecimal)
+      if (myTempData.tempInFarenheit === 0) {
         temp = '--'
         tempDecimal = '--'
       }
       retval = (
-        <div style={this.state.style}>
-          <div style={this.state.innerStyle}>
+        <div style={state.style}>
+          <div style={state.innerStyle}>
             <div style={{margin: 'auto', width: '100px'}}>
               <div style={{fontSize: '62px', float: 'left'}}>{temp}</div>
               <div style={{float: 'left', padding: '44px 2px 0px 0px'}}>{tempDecimal}</div>
             </div>
-            <div style={{clear: 'both', color: Colors.White}} >{this.state.data.name}</div>
+            <div style={{clear: 'both', color: Colors.White}} >{myTempData.sensorName}</div>
           </div>
           <div style={{color: Colors.White, fontSize: '7px', textAlign: 'right'}}>{updateTimeInMinutes}</div>
         </div>)
-    } else {
-      retval = (<div style={this.state.style}>Fetching Data</div>)
-    }
     return retval
-  }
 }
 
 TemperatureComponent.propTypes = {
-  updateIntervalInMinutes: PropTypes.number,
-  sensorName: PropTypes.string
+  sensorName: PropTypes.string,
+  myTempData: PropTypes.shape({
+      lastUpdate: PropTypes.string,
+      sensorName: PropTypes.string,
+      outdated: PropTypes.boolean,
+      tempInFarenheit: PropTypes.number
+    })
 }
 
+const getTempData = (tempData, name) => {
+  for (var i = 0; i < tempData.length; i++) {
+    var dataItem = tempData[i]
+    if (dataItem.sensorName === name) {
+        return dataItem
+    }
+  }
+}
 
-export default TemperatureComponent
+const mapStateToProps = function(state, props) {
+  return {
+    myTempData: getTempData(state.tempData, props.sensorName)
+  }
+}
+
+const reduxTemperatureComponent = connect(
+  mapStateToProps  
+)(TemperatureComponent)
+
+export default reduxTemperatureComponent
---
+++ src/client/components/tempDataFetcherComponent.js
@@ -0,0 +1,68 @@
+import { connect } from 'react-redux'
+import React from 'react'
+import PropTypes from 'prop-types'
+import ClientConfig from '../config.js'
+
+class TempDataFetcherComponent extends React.Component {
+  constructor (props) {
+    super(props)
+    var updateInterval = props.updateIntervalInMinutes * 60000
+    if (updateInterval === 0) {
+      updateInterval = 60000
+    }
+    this.getData(props.setTempData, this)
+    setInterval(() => { this.getData(props.setTempData, props.setTempDataFail, this) }, updateInterval)
+  }
+  getData (setTempData, setTempDataFail, obj) {
+    fetch(ClientConfig.hub_api_url + '/temp/current').then(function (response) {
+      if (response.status >= 400) {
+        throw new Error('Bad response from server, ' + response.status)
+      }
+      return response.json()
+    }).then(function (currentjson) {
+      var tempData = []
+      for (var i = 0; i < currentjson.length; i++) {
+        var sensor = currentjson[i]
+          var tempDataItem = {"lastUpdate":new Date().toISOString(),"sensorName":sensor.sensorName,"outdated": sensor.outdated}
+          if (!tempDataItem.outdated) {
+            tempDataItem.tempInFarenheit= sensor.tempInFarenheit
+          } else {
+            tempDataItem.tempInFarenheit = 0
+          }
+          tempData.push(tempDataItem)
+        }
+        return tempData
+      }).then(function(data) {
+        setTempData(data)
+      }).catch(function (error) {
+        setTempDataFail()
+      })
+  }
+  render () {
+    return <div></div>
+  }
+}
+
+TempDataFetcherComponent.propTypes = {
+  updateIntervalInMinutes: PropTypes.number,
+  setTempData: PropTypes.func,
+  setTempDataFail: PropTypes.func
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setTempData: function(tempData) {
+      dispatch({'type': 'SetTemps', 'tempData': tempData} )
+    },
+    setTempDataFail: function() {
+      dispatch({'type': 'SetTempsFail'} )
+    }
+  }
+}
+
+const reduxTempDataFetcherComponent = connect(
+  null,
+  mapDispatchToProps
+)(TempDataFetcherComponent)
+
+export default reduxTempDataFetcherComponent
