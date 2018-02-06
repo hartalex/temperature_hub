ba067a1ca931b68031e70ff5312f183100190322
Alex Hart <alex@hartcode.com>
2018-02-05 21:43:01 -0600

    revert back to request

--- src/server/api/data/slack.js
+++ src/server/api/data/slack.js
@@ -1,28 +1,25 @@
-
-
+const request = require('request')
 module.exports = function (slackUrl) {
   return {
     SlackPost: function (message) {
-      return new Promise(function (resolve, reject) {
-
-        var slackData = {'text': message}
-        console.log(slackData)
-        fetch(slackUrl, {
-          method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          body: slackData
-        }).then(function (response) {
-          console.log(response)
-          if (response.status >= 400) {
-            reject('Error statusCode: ' + response.status + ' body: ' + JSON.stringify(response.json()))
-          } else {
-            resolve(message)
-          }
+      return new Promise(
+        function (resolve, reject) {
+          var slackData = {'text': message}
+          request({
+            url: slackUrl,
+            method: 'POST',
+            json: true,
+            body: slackData
+          }, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+              // Sending to Slack was successful
+              resolve(message)
+            } else {
+              // Sending to Slack failed
+              reject(new Error('Error: ' + error + ' statusCode: ' + response.statusCode + ' body: ' + body))
+            }
+          })
         })
-      })
     }
   }
 }
