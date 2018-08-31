f3bd39b6de92e8a6f86d289baf4c48097e5f84e6
Alex Hart <alex@hartcode.com>
2018-08-30 22:01:59 -0500

    Add different deploys per environment

--- deployAnsible.sh
+++ deployAnsible.sh
@@ -5,4 +5,8 @@ openssl dgst -md5 ansible
 openssl dgst -md5 ansible.pub
 chmod 400 ansible
 ssh-add ansible
-ansible-playbook -i inventory.yml .ansible-deploy.yml
+case "$TRAVIS_COMMIT" in
+    *newreact* ) ansible-playbook -i ansibile/inventory.yml ansible/test-deploy-playbook.yml;;
+    *prod* ) ansible-playbook -i ansibile/inventory.yml ansible/prod-deploy-playbook.yml;;
+    * ) echo "No Deploy";;
+esac
