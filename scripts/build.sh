a9cffb467b0a9ef8de8c1ee7ec83466149ae214d
Alex Hart <alexhart@northwesternmutual.com>
2018-09-06 21:23:37 -0500

    meant to use API_URL

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,8 @@ yarn run test
 yarn global add codeclimate-test-reporter
 codeclimate-test-reporter < ../coverage/lcov.info
 case "$TRAVIS_BRANCH" in
-    "newreact" ) export CERT_NAME=hub.test.hartcode.com;;
-    "prod" ) export CERT_NAME=ssl-hub.hartcode.com;;
-    * ) echo "No Certs";;
+    "newreact" ) export API_URL=homehub.cloud.test.hartcode.com;;
+    "prod" ) export API_URL=hub.hartcode.com;;
+    * ) echo "No EnV";;
 esac
 yarn run build
