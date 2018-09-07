3b579f2fea97ab53e8cbb3eb363ec5592407a7c9
Alex Hart <alexhart@northwesternmutual.com>
2018-09-06 21:42:06 -0500

    fix coverage

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 yarn run lint 
 yarn run test 
 yarn global add codeclimate-test-reporter
-codeclimate-test-reporter < ../coverage/lcov.info
+codeclimate-test-reporter < coverage/lcov.info
 case "$TRAVIS_BRANCH" in
     "newreact" ) export API_URL=homehub.cloud.test.hartcode.com;;
     "prod" ) export API_URL=hub.hartcode.com;;
