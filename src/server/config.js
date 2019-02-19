12cc5bd654ace721225a6d8b45096ec396797120
Alex Hart <hartalex@gmail.com>
2019-02-18 21:20:34 -0600

    feat:

--- src/server/config.js
+++ src/server/config.js
@@ -1,4 +1,4 @@
-odule.exports = {
+module.exports = {
   // determines if duplicate data is inserted into the database.
   // if false then the data that hasn't changed recently will still be inserted into the database.
   // if true then only data changes will be inserted into the database.
