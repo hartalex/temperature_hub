91f6123b495d06f2273caa781332bc067d0706ae
Alex Hart <hartalex@gmail.com>
2019-02-18 21:14:28 -0600

    feat: Cleanup project

--- deployAnsible.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export ANSIBLE_HOST_KEY_CHECKING=False
-openssl dgst -md5 ansible
-openssl dgst -md5 ansible.pub
-chmod 400 ansible
-ssh-add ansible
-ansible-playbook -i inventory.yml .ansible-deploy.yml
