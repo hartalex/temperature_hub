e2405ef81d36071fccf2c2c223ed4122914bc975
Alex Hart <alex@hartcode.com>
2019-02-24 11:36:29 -0600

    feat(index.js): Turn off isSummer
    It is no longer summer

--- src/client/components/index.js
+++ src/client/components/index.js
@@ -12,38 +12,34 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import Reducers from '../reducers'
 
-let store = createStore(Reducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+let store = createStore(Reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 export function renderRoot () {
   // TODO: make this a data point, I don't want to have to manually turn this off and again
-  var isSummer = true
+  var isSummer = false
 
   var style = {
     textAlign: 'center',
-    fontFamily: 'Roboto',
-    }
+    fontFamily: 'Roboto'
+  }
   return (
     <Provider store={store}>
-    <div className='body' style={style}>
-      <DoorGraph title='Doors' duration='24h' updateIntervalInMinutes='5'/>
-      <TemperatureGraph title='Temperatures' duration='24h' updateIntervalInMinutes='5'/>
-      <Temperature sensorName='Basement'/>
-      <Temperature sensorName='Living Room'/>
-      <Temperature sensorName='Fish Tank'/>
+      <div className="body" style={style}>
+        <DoorGraph title="Doors" duration="24h" updateIntervalInMinutes="5" />
+        <TemperatureGraph title="Temperatures" duration="24h" updateIntervalInMinutes="5" />
+        <Temperature sensorName="Basement" />
+        <Temperature sensorName="Living Room" />
+        <Temperature sensorName="Fish Tank" />
 
-      <Forecast3Day updateIntervalInMinutes='60' />
-      <Temperature sensorName='Bedroom'/>
-      <Temperature sensorName='Outside'/>
-      <Temperature sensorName='Garage'/>
-      <Moon updateIntervalInMinutes='60' />
-      { !isSummer &&
-        <Menu />
-      }
-      { !isSummer &&
-        <Memory updateIntervalInMinutes='60' />
-      }
-    <TempDataFetcherComponent updateIntervalInMinutes='5'/>
-    </div>
+        <Forecast3Day updateIntervalInMinutes="60" />
+        <Temperature sensorName="Bedroom" />
+        <Temperature sensorName="Outside" />
+        <Temperature sensorName="Garage" />
+        <Moon updateIntervalInMinutes="60" />
+        {!isSummer && <Menu />}
+        {!isSummer && <Memory updateIntervalInMinutes="60" />}
+        <TempDataFetcherComponent updateIntervalInMinutes="5" />
+      </div>
     </Provider>
   )
 }
