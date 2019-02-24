9b106355e89406a86b5c5099f0c9a36574311f89
Alex Hart <alex@hartcode.com>
2019-02-24 15:34:24 -0600

    style(data.js): Make Prettier

--- src/server/api/data/data.js
+++ src/server/api/data/data.js
@@ -1,107 +1,98 @@
-import { convertToDoor } from "./models/doorModel.js";
-import { convertToTemperature } from "./models/temperatureModel.js";
-const configImport = require("../../config");
-const validation = require("./validation");
-const slackPost = require("./slack");
-const logging = require("winston");
+import { convertToDoor } from './models/doorModel.js'
+import { convertToTemperature } from './models/temperatureModel.js'
+const configImport = require('../../config')
+const validation = require('./validation')
+const slackPost = require('./slack')
+const logging = require('winston')
 
-export async function checkDupesPromise(
-  config,
-  db,
-  dbobj,
-  query,
-  sort,
-  collection,
-  dupeObject,
-  dupeProp
-) {
+export async function checkDupesPromise (config, db, dbobj, query, sort, collection, dupeObject, dupeProp) {
   return new Promise((resolve, reject) => {
     if (config.NoDuplicateData && config.NoDuplicateData === true) {
       db.queryLastData(dbobj, query, sort, collection, (err, existingData) => {
         if (err) {
-          reject(err);
+          reject(err)
         } else {
           if (existingData != null) {
-            var match = true;
-            var prop;
+            var match = true
+            var prop
             for (prop in dupeProp) {
-              match = match && existingData[prop] !== dupeObject[prop];
+              match = match && existingData[prop] !== dupeObject[prop]
             }
             if (match) {
-              resolve(dupeObject);
+              resolve(dupeObject)
             } else {
-              reject("duplicate");
+              reject('duplicate')
             }
           } else {
-            resolve(dupeObject);
+            resolve(dupeObject)
           }
         }
-      });
+      })
     } else {
-      resolve(dupeObject);
+      resolve(dupeObject)
     }
-  });
+  })
 }
 
 const insertDataPromise = (data, db, dbobj, collection, obj) => {
   return db.insertData(dbobj, collection, data).then(() => {
     return new Promise((resolve, reject) => {
-      resolve(obj);
-    });
-  });
-};
+      resolve(obj)
+    })
+  })
+}
 
-export function init(db, dbobj, config, slack) {
-  if (typeof config == "undefined") {
-    config = configImport;
+export function init (db, dbobj, config, slack) {
+  if (typeof config == 'undefined') {
+    config = configImport
   }
-  if (typeof slack == "undefined") {
-    slack = slackPost(config.slackUrl);
+  if (typeof slack == 'undefined') {
+    slack = slackPost(config.slackUrl)
   }
   return {
-    menuAdd: input => {
-      const collection = "menu";
+    menuAdd: (input) => {
+      const collection = 'menu'
       // Use connect method to connect to the Server
       return validation
-        .isNotUndefined(input, "Input")
+        .isNotUndefined(input, 'Input')
         .then(() => {
-          return validation.isNotNull(input, "Input");
+          return validation.isNotNull(input, 'Input')
         })
         .then(() => {
-          return validation.hasProperty(input, "date");
+          return validation.hasProperty(input, 'date')
         })
         .then(() => {
-          return validation.isTypeString(input.date, "date");
+          return validation.isTypeString(input.date, 'date')
         })
         .then(() => {
-          return validation.stringHasLength(input.date, "date");
+          return validation.stringHasLength(input.date, 'date')
         })
         .then(() => {
-          return validation.hasProperty(input, "firstOption");
+          return validation.hasProperty(input, 'firstOption')
         })
         .then(() => {
-          return validation.isTypeString(input.firstOption, "firstOption");
+          return validation.isTypeString(input.firstOption, 'firstOption')
         })
         .then(() => {
-          return validation.stringHasLength(input.firstOption, "firstOption");
+          return validation.stringHasLength(input.firstOption, 'firstOption')
         })
         .then(() => {
-          return validation.hasProperty(input, "secondOption");
+          return validation.hasProperty(input, 'secondOption')
         })
         .then(() => {
-          return validation.isTypeString(input.secondOption, "secondOption");
+          return validation.isTypeString(input.secondOption, 'secondOption')
         })
         .then(() => {
-          return validation.stringHasLength(input.secondOption, "secondOption");
+          return validation.stringHasLength(input.secondOption, 'secondOption')
         })
         .then(() => {
-          return validation.hasProperty(input, "otherStuff");
+          return validation.hasProperty(input, 'otherStuff')
         })
         .then(() => {
-          return validation.isTypeString(input.otherStuff, "otherStuff");
+          return validation.isTypeString(input.otherStuff, 'otherStuff')
         })
         .then(() => {
-          return validation.stringHasLength(input.otherStuff, "otherStuff");
+          return validation.stringHasLength(input.otherStuff, 'otherStuff')
         })
         .then(() => {
           return new Promise((resolve, reject) => {
@@ -111,48 +102,48 @@ export function init(db, dbobj, config, slack) {
                 date: input.date
               },
               collection,
-              result => {
+              (result) => {
                 if (result == null) {
-                  resolve(input);
+                  resolve(input)
                 } else {
-                  reject("menuItem already exists");
+                  reject('menuItem already exists')
                 }
               }
-            ); // db.queryOneData
-          });
+            ) // db.queryOneData
+          })
         }) // Promise
         .then(() => {
-          return insertDataPromise(input, db, dbobj, collection, input);
-        });
+          return insertDataPromise(input, db, dbobj, collection, input)
+        })
     },
-    tempAdd: input => {
-      const time = new Date();
+    tempAdd: (input) => {
+      const time = new Date()
       // Use connect method to connect to the Server
-      var collection = "temperatures";
+      var collection = 'temperatures'
       return validation
-        .isNotUndefined(input, "Input")
+        .isNotUndefined(input, 'Input')
         .then(() => {
-          return validation.isNotNull(input, "Input");
+          return validation.isNotNull(input, 'Input')
         })
         .then(() => {
-          if (!("utc_timestamp" in input)) {
-            input.utc_timestamp = time.toISOString();
+          if (!('utc_timestamp' in input)) {
+            input.utc_timestamp = time.toISOString()
           }
           return validation
-            .isTypeString(input.id, "id")
+            .isTypeString(input.id, 'id')
             .then(() => {
-              return validation.stringHasLength(input.id, "id");
+              return validation.stringHasLength(input.id, 'id')
             })
             .then(() => {
-              return validation.hasProperty(input, "t");
+              return validation.hasProperty(input, 't')
             })
             .then(() => {
-              return validation.isTypeNumber(input.t, "t");
+              return validation.isTypeNumber(input.t, 't')
             })
             .then(() => {
-              return convertToTemperature(input);
+              return convertToTemperature(input)
             })
-            .then(temperature => {
+            .then((temperature) => {
               return checkDupesPromise(
                 config,
                 db,
@@ -166,50 +157,44 @@ export function init(db, dbobj, config, slack) {
                 collection,
                 temperature,
                 { tempInFarenheit: 0, humidity: 0 }
-              );
+              )
             })
-            .then(temperature => {
-              return insertDataPromise(
-                temperature,
-                db,
-                dbobj,
-                collection,
-                input
-              );
-            });
-        });
+            .then((temperature) => {
+              return insertDataPromise(temperature, db, dbobj, collection, input)
+            })
+        })
     },
-    doorAdd: input => {
-      logging.log("debug", "data.DoorAdd", input);
-      const time = new Date();
+    doorAdd: (input) => {
+      logging.log('debug', 'data.DoorAdd', input)
+      const time = new Date()
       // Use connect method to connect to the Server
-      var collection = "doors";
+      var collection = 'doors'
       return validation
-        .isNotUndefined(input, "Input")
+        .isNotUndefined(input, 'Input')
         .then(() => {
-          return validation.isNotNull(input, "Input");
+          return validation.isNotNull(input, 'Input')
         })
         .then(() => {
-          if (!("utc_timestamp" in input)) {
-            input.utc_timestamp = time.toISOString();
+          if (!('utc_timestamp' in input)) {
+            input.utc_timestamp = time.toISOString()
           }
           return validation
-            .isTypeString(input.sensorId, "sensorId")
+            .isTypeString(input.sensorId, 'sensorId')
             .then(() => {
-              return validation.stringHasLength(input.sensorId, "sensorId");
+              return validation.stringHasLength(input.sensorId, 'sensorId')
             })
             .then(() => {
-              return validation.hasProperty(input, "isOpen");
+              return validation.hasProperty(input, 'isOpen')
             })
             .then(() => {
-              return validation.isTypeBoolean(input.isOpen, "isOpen");
+              return validation.isTypeBoolean(input.isOpen, 'isOpen')
             })
             .then(() => {
-              logging.log("debug", "Modeling Door", input);
-              return convertToDoor(input);
+              logging.log('debug', 'Modeling Door', input)
+              return convertToDoor(input)
             })
-            .then(door => {
-              logging.log("debug", "Checking Dupes Promise", door);
+            .then((door) => {
+              logging.log('debug', 'Checking Dupes Promise', door)
               return checkDupesPromise(
                 config,
                 db,
@@ -223,10 +208,10 @@ export function init(db, dbobj, config, slack) {
                 collection,
                 door,
                 { isOpen: 0 }
-              );
+              )
             })
-            .then(door => {
-              logging.log("debug", "querying Last Data", door);
+            .then((door) => {
+              logging.log('debug', 'querying Last Data', door)
               return new Promise((resolve, reject) => {
                 db.queryLastData(
                   dbobj,
@@ -239,25 +224,21 @@ export function init(db, dbobj, config, slack) {
                   collection,
                   (err, existingData) => {
                     if (err) {
-                      reject(err);
+                      reject(err)
                     } else {
-                      if (
-                        existingData == null ||
-                        (existingData != null &&
-                          existingData.isOpen !== door.isOpen)
-                      ) {
-                        logging.log("debug", "door has changed", door);
-                        resolve({ changed: true, door: door });
+                      if (existingData == null || (existingData != null && existingData.isOpen !== door.isOpen)) {
+                        logging.log('debug', 'door has changed', door)
+                        resolve({ changed: true, door: door })
                       } else {
-                        logging.log("debug", "door has NOT changed");
-                        resolve({ changed: false, door: door });
+                        logging.log('debug', 'door has NOT changed')
+                        resolve({ changed: false, door: door })
                       }
                     }
                   }
-                );
-              });
+                )
+              })
             })
-            .then(obj => {
+            .then((obj) => {
               // get door sensor real name from database
               return new Promise((resolve, reject) => {
                 if (obj.changed) {
@@ -266,73 +247,53 @@ export function init(db, dbobj, config, slack) {
                     {
                       sensorId: obj.door.sensorId
                     },
-                    "sensors",
+                    'sensors',
                     (err, doordb) => {
                       if (err) {
-                        reject(err);
+                        reject(err)
                       } else {
                         if (doordb == null) {
-                          logging.log(
-                            "debug",
-                            "did NOT find a name for sensor door",
-                            obj.door.sensorId
-                          );
+                          logging.log('debug', 'did NOT find a name for sensor door', obj.door.sensorId)
                         } else {
-                          logging.log(
-                            "debug",
-                            "did find a name for door sensor",
-                            doordb.name
-                          );
-                          obj.name = doordb.name;
+                          logging.log('debug', 'did find a name for door sensor', doordb.name)
+                          obj.name = doordb.name
                         }
-                        resolve(obj);
+                        resolve(obj)
                       }
                     }
-                  );
+                  )
                 } else {
-                  logging.log("debug", "door has not changed");
-                  resolve(obj);
+                  logging.log('debug', 'door has not changed')
+                  resolve(obj)
                 }
-              });
+              })
             })
-            .then(obj => {
-              logging.log("debug", "inserting Data Promise", obj.door);
-              return insertDataPromise(obj.door, db, dbobj, collection, obj);
+            .then((obj) => {
+              logging.log('debug', 'inserting Data Promise', obj.door)
+              return insertDataPromise(obj.door, db, dbobj, collection, obj)
             })
-            .then(obj => {
-              var retval;
+            .then((obj) => {
+              var retval
               if (obj.changed) {
-                var openstring = "closed";
-                var name = obj.door.sensorId;
+                var openstring = 'closed'
+                var name = obj.door.sensorId
                 if (obj.door.isOpen) {
-                  openstring = "open";
+                  openstring = 'open'
                 }
                 if (obj.name) {
-                  name = obj.name;
+                  name = obj.name
                 }
-                logging.log(
-                  "debug",
-                  "sending slack message about door",
-                  obj.door
-                );
-                retval = slack.SlackPost(
-                  name + " is now " + openstring,
-                  undefined,
-                  obj.door
-                );
+                logging.log('debug', 'sending slack message about door', obj.door)
+                retval = slack.SlackPost(name + ' is now ' + openstring, undefined, obj.door)
               } else {
-                logging.log(
-                  "debug",
-                  "NOT sending slack message about door",
-                  obj.door
-                );
+                logging.log('debug', 'NOT sending slack message about door', obj.door)
                 retval = new Promise((resolve, reject) => {
-                  resolve(obj.door);
-                });
+                  resolve(obj.door)
+                })
               }
-              return retval;
-            });
-        });
+              return retval
+            })
+        })
     }
-  };
+  }
 }
