e858ce9a75dbb8d395b02a76535efb30d543ec1e
Alex Hart <alex@hartcode.com>
2019-02-24 11:57:23 -0600

    fix(data.js): Fix bug importing database

--- src/server/api/routes/menu/menu_add.js
+++ src/server/api/routes/menu/menu_add.js
@@ -1,12 +1,12 @@
 const mongodb = require('../../db/mongodb')()
-var realData = require('../../data/data')
+import { init } from '../../data/data'
 var routeAddImport = require('../route_add')
 
 module.exports = function (req, res, done) {
   var routeAdd = routeAddImport(undefined, req.slack)
   var mydata = req.data
   if (typeof mydata === 'undefined') {
-    mydata = realData(mongodb, req.db)
+    mydata = init(mongodb, req.db)
   }
   routeAdd(mydata.menuAdd(req.body), req, res, done)
 }
