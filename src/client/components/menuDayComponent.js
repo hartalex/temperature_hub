ee8dcc398b506bbbaf476be0e72bfa227833ddec
Alex Hart <alex@hartcode.com>
2019-02-24 15:23:36 -0600

    style(menuDayComponent.js): Make Prettier

--- src/client/components/menuDayComponent.js
+++ src/client/components/menuDayComponent.js
@@ -56,55 +56,60 @@ class MenuDayComponent extends React.Component {
     }, renderInterval)
 
     this.getData(this)
-    setInterval(() => { that.getData(that) }, updateInterval)
+    setInterval(() => {
+      that.getData(that)
+    }, updateInterval)
   }
   getData (that) {
-    fetch(ClientConfig.hub_api_url + '/menu/list/' + that.state.data.date).then(function (response) {
-      if (response.status >= 400) {
-        throw new Error('Bad response from server')
-      }
-      return response.json()
-    }).then(function (currentjson) {
-      if (currentjson.length > 0) {
-        var menu = currentjson[0]
-        that.state.data.date = menu.date
-        that.state.data.firstOption = menu.firstOption
-        that.state.data.secondOption = menu.secondOption
-        that.state.data.otherStuff = menu.otherStuff
-        var style = JSON.parse(JSON.stringify(that.state.style))
-        style.backgroundColor = Colors.Black
-        style.color = Colors.White
-        that.state.style = style
-      } else {
-        that.state.data.firstOption = null
-        that.state.data.secondOption = null
-        that.state.data.otherStuff = null
+    fetch(ClientConfig.hub_api_url + '/menu/list/' + that.state.data.date)
+      .then(function (response) {
+        if (response.status >= 400) {
+          throw new Error('Bad response from server')
+        }
+        return response.json()
+      })
+      .then(function (currentjson) {
+        if (currentjson.length > 0) {
+          var menu = currentjson[0]
+          that.state.data.date = menu.date
+          that.state.data.firstOption = menu.firstOption
+          that.state.data.secondOption = menu.secondOption
+          that.state.data.otherStuff = menu.otherStuff
+          var style = JSON.parse(JSON.stringify(that.state.style))
+          style.backgroundColor = Colors.Black
+          style.color = Colors.White
+          that.state.style = style
+        } else {
+          that.state.data.firstOption = null
+          that.state.data.secondOption = null
+          that.state.data.otherStuff = null
 
-        var day = new Date(that.state.data.date).getDay() + 1
-        if (day > 6) {
-          day = 0
+          var day = new Date(that.state.data.date).getDay() + 1
+          if (day > 6) {
+            day = 0
+          }
+          if (day >= 1 && day <= 5) {
+            var styleClone = JSON.parse(JSON.stringify(that.state.style))
+            styleClone.backgroundColor = Colors.SoftYellow
+            styleClone.color = Colors.Black
+            that.state.style = styleClone
+            that.state.data.firstOption = 'No Lunch'
+          }
         }
-        if (day >= 1 && day <= 5) {
-          var styleClone = JSON.parse(JSON.stringify(that.state.style))
-          styleClone.backgroundColor = Colors.SoftYellow
-          styleClone.color = Colors.Black
-          that.state.style = styleClone
-          that.state.data.firstOption = 'No Lunch'
+        that.state.data.lastUpdate = new Date().toISOString()
+        that.setState(that.state)
+      })
+      .catch(function (error) {
+        that.state.data = {
+          date: that.state.data.date,
+          firstOption: null,
+          secondOption: null,
+          otherStuff: null,
+          day: that.state.data.day,
+          lastUpdate: '2017-01-01T00:00:00.000Z'
         }
-      }
-      that.state.data.lastUpdate = new Date().toISOString()
-      that.setState(that.state)
-    }).catch(function(error) {
-      that.state.data ={
-        date: that.state.data.date,
-        firstOption: null,
-        secondOption: null,
-        otherStuff: null,
-        day: that.state.data.day,
-        lastUpdate: '2017-01-01T00:00:00.000Z'
-      }
-      that.setState(that.state)
-    })
+        that.setState(that.state)
+      })
   }
   render () {
     var retval
@@ -115,20 +120,20 @@ class MenuDayComponent extends React.Component {
       }
       retval = (
         <div style={this.state.style}>
-          <div style={{textAlign: 'center'}}>{getWeekDay(day)}</div>
-          <ol style={{fontSize: '10px', textAlign: 'left', margin: 0, padding: '20px'}}>
-          { this.state.data.firstOption !== null &&
-          <li style={{fontSize: '12px', clear: 'left'}}>{this.state.data.firstOption}</li>
-          }
-          { this.state.data.secondOption !== null &&
-            <li style={{fontSize: '12px', padding: '5px 0', clear: 'left'}}>Or {this.state.data.secondOption}</li>
-          }
+          <div style={{ textAlign: 'center' }}>{getWeekDay(day)}</div>
+          <ol style={{ fontSize: '10px', textAlign: 'left', margin: 0, padding: '20px' }}>
+            {this.state.data.firstOption !== null && (
+              <li style={{ fontSize: '12px', clear: 'left' }}>{this.state.data.firstOption}</li>
+            )}
+            {this.state.data.secondOption !== null && (
+              <li style={{ fontSize: '12px', padding: '5px 0', clear: 'left' }}>Or {this.state.data.secondOption}</li>
+            )}
           </ol>
-          <div style={{fontSize: '10px', padding: '5px 0', clear: 'left'}}>{this.state.data.otherStuff}</div>
+          <div style={{ fontSize: '10px', padding: '5px 0', clear: 'left' }}>{this.state.data.otherStuff}</div>
         </div>
       )
     } else {
-      retval = (<div style={this.state.style}>Fetching Data</div>)
+      retval = <div style={this.state.style}>Fetching Data</div>
     }
     return retval
   }
