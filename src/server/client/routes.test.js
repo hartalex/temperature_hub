952ddabd3774aa395acbf58dcac9c94cbe69d895
Alex Hart <hartalex@gmail.com>
2019-06-03 21:55:19 -0500

    Fix for broken test in routes

--- src/server/client/routes.test.js
+++ src/server/client/routes.test.js
@@ -1,20 +1,20 @@
-const routes = require("./routes");
-var assert = require("assert");
+const routes = require('./routes');
+var assert = require('assert');
 
-describe("routes", function() {
-  describe("#function (app)", function() {
-    it("routes count 3", function() {
+describe('routes', function() {
+  describe('#function (app)', function() {
+    it('routes count 3', function() {
       var req = {};
       var cnt = 0;
       var res = {
-        render: function(txt, obj) {
-          cnt++;
-        }
+        render: jest.fn().mockReturnThis(),
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockReturnThis(),
       };
       var app = {
         get: function(obj, callback) {
           callback(req, res);
-        }
+        },
       };
       routes(app);
       assert.equal(cnt, 3);
