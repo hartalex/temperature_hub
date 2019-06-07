43f0e1d7e4be30039dbc53e487bae3a4a11665ca
Alex Hart <hartalex@gmail.com>
2019-06-07 07:03:31 -0500

    style(util.js): Prettier Style Changes

--- src/client/util.js
+++ src/client/util.js
@@ -1,21 +1,21 @@
 module.exports = {
-  timeAgo (time) {
-    var retval = Math.trunc((new Date() - new Date(time)) / 1000)
+  timeAgo(time) {
+    var retval = Math.trunc((new Date() - new Date(time)) / 1000);
     if (retval > 60) {
-      retval = Math.trunc(retval / 60)
+      retval = Math.trunc(retval / 60);
       if (retval >= 60) {
-        retval = Math.trunc(retval / 60)
-        retval = retval + ' hour'
+        retval = Math.trunc(retval / 60);
+        retval = retval + ' hour';
       } else {
-        retval = retval + ' min'
+        retval = retval + ' min';
       }
     } else {
-      retval = retval + ' sec'
+      retval = retval + ' sec';
     }
-    return retval
+    return retval;
   },
   calculateTodayTomorrowNextDay(propDay) {
-    var retval
+    var retval;
     var day = 0;
     if (propDay === 'Today') {
       day = 0;
@@ -24,8 +24,13 @@ module.exports = {
     } else if (propDay === 'NextDay') {
       day = 2;
     }
-    retval = new Date((new Date().getTime() + 24 * day * 60 * 60 * 1000) - new Date().getTimezoneOffset() * 60 * 1000).toISOString().substring(0, 10)
-    return retval
-  }
-
-}
+    retval = new Date(
+      new Date().getTime() +
+        24 * day * 60 * 60 * 1000 -
+        new Date().getTimezoneOffset() * 60 * 1000,
+    )
+      .toISOString()
+      .substring(0, 10);
+    return retval;
+  },
+};
