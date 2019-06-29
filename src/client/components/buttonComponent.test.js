2d4fcd525325fcb8f2c985f0b26c975451a2d0ba
Alex Hart <hartalex@gmail.com>
2019-06-29 07:10:41 -0500

    test(buttonComponent): Corrected date issue with snapshot
    Last Update time in the snapshots wasn't working correctly, had to mock date in order to snapshot
    the component correctly

--- src/client/components/buttonComponent.test.js
+++ src/client/components/buttonComponent.test.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ButtonComponent from './buttonComponent.js';
 import renderer from 'react-test-renderer';
+import mockdate from 'mockdate';
 
 describe('Button Component', async () => {
   it('Renders', async () => {
+    mockdate.set('2017-01-01T00:00:00.000Z');
+
     const component = renderer.create(<ButtonComponent />);
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
+  afterEach(mockdate.reset);
 });
