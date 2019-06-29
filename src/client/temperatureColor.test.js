0df6f50283481ef9c966cedb626c5e88aca838df
Alex Hart <hartalex@gmail.com>
2019-06-29 16:25:04 -0500

    test(temperatureColor.test): Fix for require failure instead of import

--- src/client/temperatureColor.test.js
+++ src/client/temperatureColor.test.js
@@ -1,30 +1,30 @@
-import Colors from "./colors";
-var assert = require("assert");
-var temperatureColor = require("./temperatureColor");
+import Colors from './colors';
+var assert = require('assert');
+import temperatureColor from './temperatureColor';
 
-describe("temperatureColor", () => {
-  it("0", () => {
+describe('temperatureColor', () => {
+  it('0', () => {
     assert.equal(temperatureColor(0), Colors.White);
   });
-  it("less than = 63 - -1", () => {
+  it('less than = 63 - -1', () => {
     assert.equal(temperatureColor(-1), Colors.SoftBlue);
   });
-  it("less than = 63 - 1", () => {
+  it('less than = 63 - 1', () => {
     assert.equal(temperatureColor(1), Colors.SoftBlue);
   });
-  it("less than = 63 - 63", () => {
+  it('less than = 63 - 63', () => {
     assert.equal(temperatureColor(63), Colors.SoftBlue);
   });
-  it("Greater than 75 - 64", () => {
+  it('Greater than 75 - 64', () => {
     assert.equal(temperatureColor(64), Colors.SoftGreen);
   });
-  it("Greater than 75 - 75", () => {
+  it('Greater than 75 - 75', () => {
     assert.equal(temperatureColor(75), Colors.SoftGreen);
   });
-  it("Greater than 75 - 76", () => {
+  it('Greater than 75 - 76', () => {
     assert.equal(temperatureColor(76), Colors.SoftRed);
   });
-  it("Greater than 75 - 100", () => {
+  it('Greater than 75 - 100', () => {
     assert.equal(temperatureColor(100), Colors.SoftRed);
   });
 });
