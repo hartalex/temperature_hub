bcee2d723b58cbbaf7021261438291bbd298260d
Alex Hart <alex@hartcode.com>
2019-07-28 20:37:43 -0500

    test(routes): Integration tests for routes.js

--- src/server/client/routes.test.js
+++ src/server/client/routes.test.js
@@ -1,21 +1,42 @@
-const routes = require('./routes');
-var assert = require('assert');
+import routes from './routes.js'
+import express from 'express'
+import request from 'supertest'
 
-describe('routes', function() {
-  describe('#function (app)', function() {
-    it('routes count 3', function() {
-      var req = {};
-      var cnt = 0;
-      var res = {
-        render: jest.fn().mockReturnThis(),
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn().mockReturnThis(),
-      };
-      var app = {
-        get: jest.fn(),
-      };
-      routes(app);
-      expect(app.get).toHaveBeenCalledTimes(4);
-    });
-  });
-});
+describe('routes', () => {
+  describe('#function (app)', () => {
+    it('routes count 4', () => {
+      const app = {
+        get: jest.fn()
+      }
+      routes(app)
+      expect(app.get).toHaveBeenCalledTimes(4)
+    })
+  })
+  describe('integration', () => {
+    const app = express()
+    app.set('view engine', 'ejs')
+
+    routes(app)
+    app.listen(8080)
+    it('/', async done => {
+      request(app)
+        .get('/')
+        .expect(200, done)
+    })
+    it('/menuEntry', async done => {
+      request(app)
+        .get('/menuEntry')
+        .expect(200, done)
+    })
+    it('/memoryEntry', async done => {
+      request(app)
+        .get('/memoryEntry')
+        .expect(200, done)
+    })
+    it('/info', async done => {
+      request(app)
+        .get('/info')
+        .expect(200, done)
+    })
+  })
+})
