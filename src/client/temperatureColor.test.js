f59c4b6c276a834c2dbae00453a16620f05b1d12
Alex Hart <alex@hartcode.com>
2019-09-08 20:36:58 -0500

    style(temperatureColor.test.js): Move import lines to the top per lint rule

--- src/client/temperatureColor.test.js
+++ src/client/temperatureColor.test.js
@@ -1,30 +1,30 @@
-import Colors from './colors';
-var assert = require('assert');
-import temperatureColor from './temperatureColor';
+import Colors from './colors'
+import temperatureColor from './temperatureColor'
+import assert from 'assert'
 
 describe('temperatureColor', () => {
   it('0', () => {
-    assert.equal(temperatureColor(0), Colors.White);
-  });
+    assert.equal(temperatureColor(0), Colors.White)
+  })
   it('less than = 63 - -1', () => {
-    assert.equal(temperatureColor(-1), Colors.SoftBlue);
-  });
+    assert.equal(temperatureColor(-1), Colors.SoftBlue)
+  })
   it('less than = 63 - 1', () => {
-    assert.equal(temperatureColor(1), Colors.SoftBlue);
-  });
+    assert.equal(temperatureColor(1), Colors.SoftBlue)
+  })
   it('less than = 63 - 63', () => {
-    assert.equal(temperatureColor(63), Colors.SoftBlue);
-  });
+    assert.equal(temperatureColor(63), Colors.SoftBlue)
+  })
   it('Greater than 75 - 64', () => {
-    assert.equal(temperatureColor(64), Colors.SoftGreen);
-  });
+    assert.equal(temperatureColor(64), Colors.SoftGreen)
+  })
   it('Greater than 75 - 75', () => {
-    assert.equal(temperatureColor(75), Colors.SoftGreen);
-  });
+    assert.equal(temperatureColor(75), Colors.SoftGreen)
+  })
   it('Greater than 75 - 76', () => {
-    assert.equal(temperatureColor(76), Colors.SoftRed);
-  });
+    assert.equal(temperatureColor(76), Colors.SoftRed)
+  })
   it('Greater than 75 - 100', () => {
-    assert.equal(temperatureColor(100), Colors.SoftRed);
-  });
-});
+    assert.equal(temperatureColor(100), Colors.SoftRed)
+  })
+})
