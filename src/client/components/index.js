55f21c860582ea2060f552f064acda2e3e90f99f
Alex Hart <alex@hartcode.com>
2019-09-10 20:20:00 -0500

    feat(buttonComponent): Remove buttonComponent

--- src/client/components/index.js
+++ src/client/components/index.js
@@ -1,31 +1,30 @@
-import DoorGraph from './doorGraphComponent.js';
-import TemperatureGraph from './temperatureGraphComponent.js';
-import Temperature from './tempComponent.js';
-import TempDataFetcherComponent from './tempDataFetcherComponent.js';
-import Menu from './menuComponent.js';
+import DoorGraph from './doorGraphComponent.js'
+import TemperatureGraph from './temperatureGraphComponent.js'
+import Temperature from './tempComponent.js'
+import TempDataFetcherComponent from './tempDataFetcherComponent.js'
+import Menu from './menuComponent.js'
 //import Forecast3Day from './forecast3DayComponent.js'
 //import Moon from './moonComponent.js'
-import Memory from './memoryComponent.js';
+import Memory from './memoryComponent.js'
 
-import React from 'react';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import Reducers from '../reducers';
-import Button from './buttonComponent.js';
+import React from 'react'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import Reducers from '../reducers'
 
 let store = createStore(
   Reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 export function renderRoot() {
   // TODO: make this a data point, I don't want to have to manually turn this off and again
-  var isSummer = true;
+  var isSummer = true
 
   var style = {
     textAlign: 'center',
-    fontFamily: 'Roboto',
-  };
+    fontFamily: 'Roboto'
+  }
   return (
     <Provider store={store}>
       <div className="body" style={style}>
@@ -37,7 +36,6 @@ export function renderRoot() {
         <Temperature sensorName="Bedroom" />
         <Temperature sensorName="Outside" />
         <Temperature sensorName="Garage" />
-        <Button updateIntervalInSeconds="60" />
         <DoorGraph title="Doors" duration="24h" updateIntervalInMinutes="5" />
         <TemperatureGraph
           title="Temperatures"
@@ -47,5 +45,5 @@ export function renderRoot() {
         <TempDataFetcherComponent updateIntervalInMinutes="5" />
       </div>
     </Provider>
-  );
+  )
 }
